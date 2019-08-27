bb91b7f3639029f1c529305566907962f0242546
unknown <lucasfoulkes@gmail.com>
2019-08-27 05:45:34 -0400

    update to login

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// i think i can get away with having all the libraries here
 const express = require('express')
 const path = require('path')
 const { get } = require('request')
@@ -11,10 +12,12 @@ app.use(express.urlencoded({ extended: true }))
 
 const viewsDir = path.join(__dirname, '')
 app.use(express.static(viewsDir))
-app.use(express.static(path.join(__dirname, './labeled_images')))
-app.use(express.static(path.join(__dirname, './models')))
+// not sure if im using this correctly
+app.use(express.static(path.join(__dirname, 'public')))
 
-require("./routes/htmlRoutes")(app);
+
+require("./routes/html-routes")(app);
+require("./routes/post-routes")(app);
 
 
 app.listen(PORT, function () {
@@ -24,46 +27,6 @@ app.listen(PORT, function () {
 
 // ==========================================================================
 
-var multer = require('multer')
-var dir = "";
-
-var mkdirp = require('mkdirp');
-
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, './labeled_images'))
-  },
-  filename: function (req, file, cb) {
-    cb(null, "1.jpg")
-  }
-})
-
-const fs = require('fs-extra')
-const upload = multer({ storage: storage })
-
-app.post('/profile', upload.single('avatar'), function (req, res, next) {
-  // <!-- need to add something so that when this happens an entry is added to a mysql database -->
-  console.log(req.body.username)
-  dir = req.body.username.toString()
-  mkdirp(path.join(__dirname, './labeled_images', dir), function (err) {
-    if (err) console.error(err)
-    else {
-      console.log('pow!')
-      fs.move(
-        path.join(__dirname, './labeled_images', '1.jpg'),
-        path.join(__dirname, './labeled_images',dir, '1.jpg'), { overwrite: true }, err => {
-          if (err) return console.error(err)
-          console.log('success!')
-        })
-    }
-  });
-})
-
-// ==========================================================================
-
-fs.readdirSync("labeled_images/").forEach(file => {
-  console.log(file);
-});
 
 
 
